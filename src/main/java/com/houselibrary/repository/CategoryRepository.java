faf6499c3c63e6a8b4e7537c0447e26c3726bf26
KaKi458 <kicinskikacper@gmail.com>
2022-10-18 19:50:24 +0200

    List instead of Set

--- src/main/java/com/houselibrary/repository/CategoryRepository.java
+++ src/main/java/com/houselibrary/repository/CategoryRepository.java
@@ -4,6 +4,11 @@ import com.houselibrary.model.Category;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends CrudRepository<Category, Integer> {
+
+    @Override
+    List<Category> findAll();
 }
